73c51f6d79725638026e03ebc5b5b0ca6a1e94e1
Marek Hobler <marek@whitehats.pl>
2016-03-21 22:44:11 +0100

    add battery drained screen
    Stolen from x-magic/kindle-weather-display

--- kindle/display-weather.sh
+++ kindle/display-weather.sh
@@ -2,12 +2,28 @@
 
 cd "$(dirname "$0")"
 
-rm -f display.png
+#clear the display to avoid shadows
 eips -c
 eips -c
 
-if wget -q http://server/path/to/display.png; then
-	eips -g display.png
+# Check battery capacity
+BATTERY=`grep -o "[0-9]*" /sys/devices/system/yoshi_battery/yoshi_battery0/battery_capacity`
+CURRENT=`cat /sys/devices/system/yoshi_battery/yoshi_battery0/battery_current`
+
+if [ $BATTERY -le 10 ] && [ $CURRENT -le 0 ]; then
+	# Show battery drained image
+    eips -g battery-drained.png
 else
-	eips -g weather-image-error.png
+	# Stop services to turn off screensavers and gui
+	/etc/init.d/framework stop
+	/etc/init.d/powerd stop
+	# Remove old file
+	rm weather-display.png
+	# Download updated weather picture
+	if wget "http://Your-Website-Domain/weather-display.png"; then
+		eips -g weather-display.png
+	else
+		eips -g weather-error.png
+	fi
 fi
+
